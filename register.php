b34aa887be97e691f48141addac334628a064399
enamulrafti <enamul.rafti@gmail.com>
2022-03-26 03:14:11 +0600

    register.php login session and other php file included

--- register.php
+++ register.php
@@ -1,5 +1,21 @@
 <!-- This is signup page design  -->
+<?php
+    include("includes/config.php");
+    include("includes/classes/Account.php");
+    include("includes/classes/Constants.php");
  
+    $account = new Account($con);
+ 
+    include("includes/handlers/register-handler.php");
+    // include("includes/handlers/login-handler.php");
+ 
+    function getInputValue($name) {
+        if(isset($_POST[$name])) {
+            echo $_POST[$name];
+        }
+    }
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
