5567628c9a8016fe57cc7383aedfac807c849bc0
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-26 14:27:39 +0600

    reading user password and updating it

--- updateDetails.php
+++ updateDetails.php
@@ -16,6 +16,13 @@ include("includes/includedFiles.php");
 		<span class="message"></span>
 		<button class="button" onclick="updateDescription('description')">SAVE</button>
 	</div>	
-	
+	<div class="container">
+		<h2>PASSWORD</h2>
+		<input type="password" class="oldPassword" name="oldPassword" placeholder="Enter Current password">
+		<input type="password" class="newPassword1" name="newPassword1" placeholder="Enter New password">
+		<input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm New password">
+		<span class="message"></span>
+		<button class="button" onclick="updatePassword('oldPassword', 'newPassword1', 'newPassword2')">SAVE</button>
+	</div>
 
 </div>
