9229b4ff99c29e6291e05a486a26f4758e51a410
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-26 14:16:28 +0600

    an include files to access all the php files created

---
+++ includes/includedFiles.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+	include("includes/config.php");
+	include("includes/classes/User.php");
+	include("includes/classes/Artist.php");
+	include("includes/classes/Album.php");
+	include("includes/classes/Song.php");
+	include("includes/classes/Playlist.php");
+	if(isset($_GET['userLoggedIn'])) {
+		$userLoggedIn = new User($con, $_GET['userLoggedIn']);
+	}
+	else {
+		echo "Username variable was not passed into page. Check the openPage JS function";
+		exit();
+	}
+}
+else {
+	
+	$url = $_SERVER['REQUEST_URI'];
+	echo "<script>openPage('$url')</script>";
+	exit();
+}
+
+?>
