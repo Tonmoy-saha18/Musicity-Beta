0c95c5d1561d3ecdc55ed8f2c811a127d5830135
unknown <mahmudyea@gmail.com>
2022-03-26 13:28:26 +0600

    album-handler is completed

--- includes/handlers/album-handler.php
+++ includes/handlers/album-handler.php
@@ -13,7 +13,33 @@
                 $artist_id = $user['id'];
             }
             //checking the image is valid or not/the choosen file is image or not
-            
+            $image_info = @getimagesize($_FILES['album-picture']['tmp_name']);
+            //if the image file is valid it will insert the path into database and upload the file
+            if($image_info){
+                $title = $_POST['title'];
+                $recieved_file = $_FILES['album-picture']; //recieving the file
+                $image_name = $recieved_file['name']; //take the file name
+                $from = $recieved_file['tmp_name']; // take the path where it temporaily store in database
+                $to = "../../assets/images/artwork/$image_name";
+                $f_to = "assets/images/artwork/$image_name";
+ 
+                // inserting the details into albums table
+                $insert_details = "INSERT INTO albums VALUES(NULL,'$title','$f_to',$artist_id)";
+                $con->exec($insert_details);
+ 
+                move_uploaded_file($from,$to);//moving the file to upload folder
+                ?>
+                    <script>
+                    setTimeout(() => {
+                        alert('Album created successfully')
+                    }, 500);
+ 
+                    setTimeout(() => {
+                        location.assign('../../index.php')
+                    }, 500)
+                    // location.assign('../../index.php')
+                   
+                    </script>
                 <?php
             }
             else{
