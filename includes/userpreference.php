cb471e70923a3e7b4cde5dffcdee07be73757c4a
enamulrafti <enamul.rafti@gmail.com>
2022-03-26 10:35:06 +0600

    genre selection part update 2 added

--- includes/userpreference.php
+++ includes/userpreference.php
@@ -16,3 +16,34 @@
             <link rel="stylesheet" href="assets/css/userpreferance.css">
             <title>User Preferance</title>
         </head>
+        <body>
+            <div class="maincontainer">
+                    <h4>What type of music you want to listen?</h4>
+                    <form action="includes/handlers/userpreference-handler.php" method="get" class="preferanceform">
+                        <?php
+                            $query = "SELECT * FROM genre";
+                            $returnobj = $con->query($query);
+                            $table = $returnobj->fetchAll();
+                            foreach ($table as $gnr){
+                                ?>
+                                    <p>
+                                        <input type="checkbox" id="<?php echo $gnr['name']; ?>" value="<?php echo $gnr['name']; ?>" name="userchoice[]">
+                                        <label for="<?php echo $gnr['name']; ?>"> <?php echo $gnr['name']; ?></label>
+                                    </p>
+                                <?php
+                            }
+ 
+                        ?>
+                        <button class='btn' name="submitButton">Submit</button>
+                    </form>
+            </div>
+        </body>
+        </html>
+        <?php
+    }
+    else{
+        ?>
+            <script>location.assign('signup.php')</script>
+        <?php
+    }
+?>
