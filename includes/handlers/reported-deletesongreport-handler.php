30c5da55589af7d20633d5150ab30cb86db6aa7a
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-03-27 01:41:53 +0600

    delete reports and song code is added

--- includes/handlers/reported-deletesongreport-handler.php
+++ includes/handlers/reported-deletesongreport-handler.php
@@ -6,12 +6,18 @@
     if(isset($_SESSION['adminLoggedIn']) && !empty($_SESSION['adminLoggedIn'])){
         //
         if(isset($_GET['songid']) && !empty($_GET['songid']) && isset($_GET['reportid']) && !empty($_GET['reportid'])){
-            
+            $song_id = $_GET['songid'];
+            $reportid = $_GET['reportid'];
+            $query = "DELETE FROM songs WHERE id=$song_id";
+            $con->exec($query);
+            $query = "DELETE FROM reports WHERE id=$reportid OR song_id=$song_id";
+            $con->exec($query);
+
+            ?>
             <?php
         }
         else{
             ?>
-                
             <?php
         }
     }
