ebeb35bce9d5a2bc01b97a925086a7fabb99c174
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-26 13:53:02 +0600

    all crud methods added to user class

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -13,6 +13,47 @@
 			return $this->username;
 		}
 
-		
+		public function getUserId() {
+			$query = "SELECT id FROM users WHERE username='$this->username'";
+			//echo ($this->username);
+			$row =  $this->con->query($query);
+			$data = $row->fetchAll();
+			$id = $data[0];
+			return $id['id'];
+		}
+		public function getUserBalance() {
+			$query = "SELECT amount FROM users WHERE username='$this->username'";
+			//echo ($this->username);
+			$row =  $this->con->query($query);
+			$data = $row->fetch();
+			return $data['amount'];
+		}
+
+		public function getEmail() {
+			$query = "SELECT email FROM users WHERE username='$this->username'";
+			$row = $this->con->query($query);
+			$data = $row->fetchAll();
+			$email=$data[0];
+			return $email['email'];
+
+		}
+		public function getDescription() {
+			$query = "SELECT description FROM users WHERE username='$this->username'";
+			$row = $this->con->query($query);
+			$data = $row->fetchAll();
+			$description=$data[0];
+			return $description['description'];
+
+		}
+
+
+		public function getFirstAndLastName() {
+			$query = "SELECT concat(first_name, ' ', last_name) as 'name'  FROM users WHERE username='$this->username'";
+			$row = $this->con->query($query);
+			$data = $row->fetchAll();
+			$name=$data[0];
+			return $name['name'];
+		}
+
 	}
 ?>   
