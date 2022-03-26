6990e16606c1233564be4f6f11c5a0094c005ac6
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-26 14:09:42 +0600

    condition added for updating description

--- includes/handlers/ajax/updateDescription.php
+++ includes/handlers/ajax/updateDescription.php
@@ -6,6 +6,18 @@ if(!isset($_POST['username'])) {
 	exit();
 }
 
+if(isset($_POST['description']) && $_POST['description'] != "") {
 
+	$username = $_POST['username'];
+	$description = $_POST['description'];
+
+	$updateQuery = "UPDATE users SET description = '$description' WHERE username='$username'";
+	$con->exec($updateQuery);
+	echo "Update successful";
+
+}
+else {
+	echo "You must provide an email";
+}
 
 ?>
