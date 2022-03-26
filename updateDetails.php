f33fd385c576bff61296e43b61d292acee6c8469
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-26 14:25:55 +0600

    read user email and update it

--- updateDetails.php
+++ updateDetails.php
@@ -2,3 +2,14 @@
 include("includes/includedFiles.php");
 ?>
 
+<div class="userDetails">
+
+	<div class="container borderBottom">
+		<h2>EMAIL</h2>
+		<input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail(); ?>">
+		<span class="message"></span>
+		<button class="button" onclick="updateEmail('email')">SAVE</button>
+	</div>
+	
+
+</div>
