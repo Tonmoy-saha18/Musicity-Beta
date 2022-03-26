000e537aea98d6e225d6c727a572ba0f78d8a3fc
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-26 13:56:42 +0600

    all methods and functions added in artist class

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -9,6 +9,35 @@
 			$this->id = $id;
 		}
 
+		public function getId() {
+			return $this->id;
+		}
+
+		public function getName() {
+			$artistQuery = "SELECT concat(first_name, ' ', last_name) as name FROM users WHERE id='$this->id'";
+			$artist = $this->con->query($artistQuery);
+			$data = $artist->fetchAll();
+			$name=$data[0] ?? null;
+			return $name['name'] ?? null;
+		}
+
+		public function getDescription() {
+			$artistQuery = "SELECT description FROM users WHERE id='$this->id'";
+			$artist = $this->con->query($artistQuery);
+			$data = $artist->fetchAll();
+			$name=$data[0] ?? null;
+			return $name['description'] ?? null;
+		}
 		
+		public function getSongIds() {
+
+			$query = "SELECT s.id as id FROM `songs` as s JOIN albums as u ON u.id = s.album_id WHERE u.artist_id='$this->id' ORDER BY plays ASC";
+
+			$data= $this->con->query($query);
+			$row= $data->fetchAll();
+			$ids=array_column($row, 'id');
+			return $ids;
+
+		}
 	}
 ?>
