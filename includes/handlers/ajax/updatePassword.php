34fdb459016a77a6653442081680432a5a58abc1
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-26 14:13:13 +0600

    all necessary functions added for handling password update

--- includes/handlers/ajax/updatePassword.php
+++ includes/handlers/ajax/updatePassword.php
@@ -16,5 +16,40 @@ if($_POST['oldPassword'] == "" || $_POST['newPassword1'] == ""  || $_POST['newPa
 	exit();
 }
 
+$username = $_POST['username'];
+$oldPassword = $_POST['oldPassword'];
+$newPassword1 = $_POST['newPassword1'];
+$newPassword2 = $_POST['newPassword2'];
+
+$oldMd5 = md5($oldPassword);
+
+$passwordCheck =  "SELECT * FROM users WHERE username='$username' AND pasword='$oldMd5'";
+$data  = $con->query($passwordCheck);
+
+if($data->rowCount()!= 1) {
+	echo "Password is incorrect";
+	exit();
+}
+
+if($newPassword1 != $newPassword2) {
+	echo "Your new passwords do not match";
+	exit();
+}
+
+if(preg_match('/[^A-Za-z0-9]/', $newPassword1)) {
+	echo "Your password must only contain letters and/or numbers";
+	exit();
+}
+
+if(strlen($newPassword1) > 30 || strlen($newPassword1) < 5) {
+	echo "Your username must be between 5 and 30 characters";
+	exit();
+}
+
+$newMd5 = md5($newPassword1);
+
+$query = "UPDATE users SET pasword='$newMd5' WHERE username='$username'";
+$con->exec($query);
+echo "Update successful";
 
 ?>
