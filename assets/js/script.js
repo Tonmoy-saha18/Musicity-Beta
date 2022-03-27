cead521e2e8b3fdd0a5bf9c9393255195d952d5e
ahmed123doeswork <ahm3dxb@gmail.com>
2022-03-27 22:52:05 +0600

    js file created

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+
+function openPage(url) {
+	if(timer != null) {
+		clearTimeout(timer);
+	}
+	if(url.indexOf("?") == -1) {
+		url = url + "?";
+	}
+
+	var encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
+	$("#mainContent").load(encodedUrl);
+	$("body").scrollTop(0);
+	history.pushState(null, null, url);
+}
+
+
+function updateEmail(emailClass) {
+	var emailValue = $("." + emailClass).val();
+
+	$.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn})
+	.done(function(response) {
+		$("." + emailClass).nextAll(".message").text(response);
+	})
+
+
+}
+
+function updateDescription(descriptionClass){
+	var dValue = $("." + descriptionClass).val();
+
+	$.post("includes/handlers/ajax/updateDescription.php", { description: dValue, username: userLoggedIn})
+	.done(function(response) {
+		$("." + descriptionClass).nextAll(".message").text(response);
+	})
+}
+
+function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
+	var oldPassword = $("." + oldPasswordClass).val();
+	var newPassword1 = $("." + newPasswordClass1).val();
+	var newPassword2 = $("." + newPasswordClass2).val();
+
+	$.post("includes/handlers/ajax/updatePassword.php", 
+		{ oldPassword: oldPassword,
+			newPassword1: newPassword1,
+			newPassword2: newPassword2, 
+			username: userLoggedIn})
+
+	.done(function(response) {
+		$("." + oldPasswordClass).nextAll(".message").text(response);
+	})
+
+
+}
