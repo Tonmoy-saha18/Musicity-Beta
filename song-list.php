5e36a545d3c48d17dd24c08b3b71d4cf37de08d1
Nila <adritanila64658@gmail.com>
2022-03-27 19:39:26 +0600

    song list boostrap and linking file added

--- song-list.php
+++ song-list.php
@@ -4,7 +4,22 @@
     // checkig whelther admin is logged in or not
     if(isset($_SESSION['adminLoggedIn']) && !empty($_SESSION['adminLoggedIn'])){
         ?>
-        
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+            <link rel="shortcut icon" type="/image/png" href="assets/images/titleimage.png">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+            <link rel="stylesheet" href="assets/css/song-list.css">
+            <title>Song-list</title>
+        </head>
+        <body>
+           
+        </body>
+        </html>
         <?php
     }
     else{
