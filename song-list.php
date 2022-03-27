5141c15aea80772007bfd76a30c1811eec9570d1
Nila <adritanila64658@gmail.com>
2022-03-27 19:40:22 +0600

    song list navbar and song fetching query added

--- song-list.php
+++ song-list.php
@@ -17,7 +17,32 @@
             <title>Song-list</title>
         </head>
         <body>
-           
+            <div class="fluid-container">
+                <nav class="navbar navbar-light bg-success" style="border-bottom:2px solid black; position:sticky; top:0;   width:100%;">
+                    <div class="container-fluid">
+                        <a class="" href="admin-view.php" style="text-decoration: none; color:black; font-weight:bold;"><i class="fas fa-backward"></i> Back</a>
+                        <a class="navbar-brand mx-auto" style="text-decoration: none; color:black; font-weight:bold;"href="song-list.php">Song List</a>
+                    </div>
+                </nav>
+                <?php
+                    $query = "SELECT * FROM songs";
+                    $returnobj = $con->query($query);
+                    $table = $returnobj->fetchAll();
+                    foreach ($table as $song){
+                        ?>
+                         <div class="song-list">
+                            <div class="indiv">
+                                <h5><?php echo $song['id']; ?></>
+                                <h5><?php echo $song['title']; ?></h5><br>
+                                <audio controls>
+                                    <source src="<?php echo $song['song_path'];?>" type="audio/mp3">
+                                </audio>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                ?>
+            </div>
         </body>
         </html>
         <?php
