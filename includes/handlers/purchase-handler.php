2a9cd9d95dc0cbfc4560fe112f17905bd6e75252
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-03-27 17:38:31 +0600

    code added for checking whelther all the essential data is recieved or not through the url

--- includes/handlers/purchase-handler.php
+++ includes/handlers/purchase-handler.php
@@ -2,7 +2,21 @@
     session_start();
     include("../config.php");
     if(isset($_SESSION['userLoggedIn']) && !empty($_SESSION['userLoggedIn'])){
-        
+        if(isset($_GET['sid']) && !empty($_GET['sid']) && isset($_GET['pname']) && !empty($_GET['pname'])){
+            
+            }
+            else{
+                //playlist doesn't exist so we will have to show the error to the users
+                ?>
+                    
+                <?php
+            }
+        }
+        else{
+            ?>
+                <script>location.assign('../../index.php')</script>
+            <?php
+        }
     }
     else{
         ?>
