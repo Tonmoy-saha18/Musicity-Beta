b23931612097ed7334f38f479f43f6553f558352
ahmed123doeswork <ahm3dxb@gmail.com>
2022-04-14 23:35:18 +0600

    minor changes made

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -4,9 +4,9 @@ if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
 	include("includes/config.php");
 	include("includes/classes/User.php");
 	include("includes/classes/Artist.php");
-	include("includes/classes/Album.php");
-	include("includes/classes/Song.php");
-	include("includes/classes/Playlist.php");
+	// include("includes/classes/Album.php");
+	// include("includes/classes/Song.php");
+	// include("includes/classes/Playlist.php");
 	if(isset($_GET['userLoggedIn'])) {
 		$userLoggedIn = new User($con, $_GET['userLoggedIn']);
 	}
@@ -16,7 +16,9 @@ if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
 	}
 }
 else {
-	
+	include("includes/header.php");
+	include("includes/footer.php");
+
 	$url = $_SERVER['REQUEST_URI'];
 	echo "<script>openPage('$url')</script>";
 	exit();
