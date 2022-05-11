315f0e0a7d839f6e32f9e5b4a1163325aa721b8f
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-11 23:50:41 +0600

    Create Song Class and add some variables in that class

---
+++ includes/classes/Song.php
@@ -0,0 +1,13 @@
+<?php
+	class Song {
+
+		private $con;
+		private $id;
+		private $mysqliData;
+		private $title;
+		private $artistId;
+		private $albumId;
+		private $genre;
+		private $duration;
+		private $path;
+		private $setCharge;
