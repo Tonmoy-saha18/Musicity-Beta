a83440bbcf880d1fd04d0895430102bcda33eb7b
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-11 23:51:42 +0600

    Constructor added for song class

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -11,3 +11,18 @@
 		private $duration;
 		private $path;
 		private $setCharge;
+
+        public function __construct($con, $id) {
+			$this->con = $con;
+			$this->id = $id;
+			$query = "SELECT * FROM songs WHERE id='$this->id'";
+			$returnobj=$this->con->query($query);
+			$data=$returnobj->fetch(); 
+			$this->mysqliData = $data;
+			$this->title = $this->mysqliData['title'];
+			$this->albumId = $this->mysqliData['album_id'];
+			$this->genre = $this->mysqliData['genre_id'];
+			$this->duration = $this->mysqliData['duration'];
+			$this->path = $this->mysqliData['song_path'];
+			$this->path = $this->mysqliData['set_charge'];
+		}
