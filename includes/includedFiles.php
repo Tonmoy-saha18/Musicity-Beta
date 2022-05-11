1e127dde5a2f574e2caf678eb04fed401ca774e8
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-11 23:47:17 +0600

    add two lines in includeFiles.php

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -4,8 +4,8 @@ if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
 	include("includes/config.php");
 	include("includes/classes/User.php");
 	include("includes/classes/Artist.php");
-	// include("includes/classes/Album.php");
-	// include("includes/classes/Song.php");
+	include("includes/classes/Album.php");
+	include("includes/classes/Song.php");
 	// include("includes/classes/Playlist.php");
 	if(isset($_GET['userLoggedIn'])) {
 		$userLoggedIn = new User($con, $_GET['userLoggedIn']);
