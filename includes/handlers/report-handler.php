2c7ac07ee898f6d4997e9827282020d1a8668237
rsnila <adritanila64658@gmail.com>
2022-05-13 01:18:12 +0600

    Code added for inserting reports

--- includes/handlers/report-handler.php
+++ includes/handlers/report-handler.php
@@ -3,7 +3,18 @@
     include("../config.php");
     if(isset($_SESSION['userLoggedIn']) && !empty($_SESSION['userLoggedIn'])){
         if(isset($_GET['userid']) && !empty($_GET['userid']) && isset($_GET['songid']) && !empty($_GET['songid'])){
-            
+            //taking the details
+            $userid = $_GET['userid'];
+            $song_id = $_GET['songid'];
+            $date = date("Y-m-d H:i:s");
+            $message = $_GET['msg'];
+            echo $message;
+ 
+            $query = "INSERT INTO reports VALUES(NULL,$userid,$song_id,'$date','$message')";
+            $con->exec($query);
+            ?>
+                
+            <?php
         }
         else{
             ?>
