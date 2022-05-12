e43b83f344a339b3433335ad35b6f6cd5a6696cf
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-13 01:29:27 +0600

    remove some line for update purpose

--- home.php
+++ home.php
@@ -12,31 +12,5 @@ if(isset($_GET['userLoggedIn'])) {
 
 <div class="gridViewContainer">
 
-	<?php
-		$id=$userLoggedIn->getUserId();
-		$query = "SELECT a.id, a.artwork_path, a.title 
-					FROM albums as a
-					JOIN songs as s
-						 on s.album_id=a.id 
-					JOIN user_preference as u
-					     on u.genre_id = s.genre_id
-					WHERE u.user_id=$id
-					GROUP BY a.id
-					ORDER BY RAND() LIMIT 50";
-		$album = $con->query($query);
-		$data = $album->fetchAll();	
-		 foreach($data AS $row){
-			$img = $row['artwork_path'];
-		 	echo "<div class='gridViewItem'>
-					<a href='album.php?id=" . $row['id'] . "'>
-						<img src='" . $img . "'>
-
-						<div class='gridViewInfo'>"
-							. $row['title'] .
-						"</div>
-					</span>
-
-				</div>";
-		 }
-		?>
+	
 </div>
