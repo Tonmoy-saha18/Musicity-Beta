749497e10571814420a15167f61f4c67dd5f1c0a
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-12 12:29:07 +0600

    Complete uploadpodcast.php front page

--- uploadpodcast.php
+++ uploadpodcast.php
@@ -11,6 +11,7 @@
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
             <link rel="shortcut icon" type="/image/png" href="assets/images/titleimage.png">
             <title>UploadPodcast</title>
             <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
@@ -18,20 +19,22 @@
             <link rel="stylesheet" href="assets/css/uploadpodcast.css">
         </head>
         <body>
-        <div class='login'>
-            <h2>Upload Podcast</h2>
-            <form action="" method="post">
-                <input name='username' placeholder='Username' type='text'>
-                <input id='pw' name='password' placeholder='Password' type='password'>
-                <input name='email' placeholder='E-Mail Address' type='text'>
-                <div class='agree'>
-                    <input id='agree' name='agree' type='checkbox'>
-                    <label for='agree'></label>Accept rules and conditions
-                </div>
-                <input class='animated' type='submit' value='Register'>
-            </form>
-            <a class='forgot' href='#'>Already have an account?</a>
-        </div>
+            <div class='login'>
+                <h2>Upload Podcast</h2>
+                <form action="includes/handlers/UploadPodcastHandler.php" method="post" enctype="multipart/form-data">
+                    <i class="fas fa-music"></i>
+                    <input name='title' placeholder='Podcast Title' type='text' required>
+                    <br>
+                    <i class="fas fa-clock"></i>
+                    <input name='duration' placeholder='Podcast Duration (mm:ss)' type='text' required>
+                    <input name='file-path' placeholder='choosepodacast' type='file' required>
+                    <!-- <input id='pw' name='password' placeholder='Password' type='password'>
+                    <input name='email' placeholder='E-Mail Address' type='text'> -->
+                    <div class='agree'>
+                    </div>
+                    <input class='animated' type='submit' value='Upload'>
+                </form>
+            </div>
         </body>
         </html>
         <?php
