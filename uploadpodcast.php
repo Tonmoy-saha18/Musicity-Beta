5d82cc022ba26a584e0d0ad957f90cf34f7394ce
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-12 11:36:54 +0600

    Add uploadpodcast file and add some initial code

---
+++ uploadpodcast.php
@@ -0,0 +1,18 @@
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="assets/css/uploadpodcast.css">
+<div class='login'>
+  <h2>Upload Podcast</h2>
+  <form action="" method="post">
+    <input name='username' placeholder='Username' type='text'>
+    <input id='pw' name='password' placeholder='Password' type='password'>
+    <input name='email' placeholder='E-Mail Address' type='text'>
+    <div class='agree'>
+        <input id='agree' name='agree' type='checkbox'>
+        <label for='agree'></label>Accept rules and conditions
+    </div>
+    <input class='animated' type='submit' value='Register'>
+  </form>
+  <a class='forgot' href='#'>Already have an account?</a>
+</div>
