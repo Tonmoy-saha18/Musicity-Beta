47762f5c42ccac9d7526b79569f360dd6529b0b1
enamulrafti <enamul.rafti@gmail.com>
2022-05-13 16:40:06 +0600

    add playlist necessary method

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -23,7 +23,41 @@
             $this->owner = $data['owner_id'];
         }
  
-        
+        public function getId() {
+            return $this->id;
+        }
+ 
+        public function getName() {
+            return $this->name;
+        }
+ 
+        public function getOwner() {
+            $query = "SELECT username FROM users WHERE id='$this->owner'";
+            $data  = $this->con->query($query);
+            $row=$data->fetchAll();
+            $id=$row[0];
+            return $id['username'];
+        }
+ 
+        public function getNumberOfSongs() {
+            $query = "SELECT song_id FROM playlist_song WHERE playlist_id='$this->id'";
+            $data  = $this->con->query($query);
+            return $data->rowCount();
+        }
+ 
+        public function getSongIds() {
+ 
+            $query = "SELECT song_id FROM playlist_song WHERE playlist_id='$this->id' ORDER BY playlist_order ASC";
+ 
+            $data= $this->con->query($query);
+            $row= $data->fetchAll();
+            return $row;
+ 
+ 
+        }
+ 
+       
+ 
  
  
  
