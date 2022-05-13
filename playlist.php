a1036ca497e8d016f625b8ac1128286678567ede
enamulrafti <enamul.rafti@gmail.com>
2022-05-13 16:54:17 +0600

    add playlist frontend update 2 _entity info method

--- playlist.php
+++ playlist.php
@@ -11,6 +11,25 @@ $playlist = new Playlist($con, $playlistId);
 $owner = new User($con, $playlist->getOwner());
 ?>
  
+<div class="entityInfo">
+ 
+    <div class="leftSection">
+        <div class="playlistImage">
+            <img src="assets/images/icons/playlist.png">
+        </div>
+    </div>
+ 
+    <div class="rightSection">
+        <h2><?php echo $playlist->getName(); ?></h2>
+        <p>By <?php echo $playlist->getOwner(); ?></p>
+        <p><?php echo $playlist->getNumberOfSongs(); ?> songs</p>
+        <button class="button" onclick="deletePlaylist('<?php echo $playlistId; ?>')">DELETE PLAYLIST</button>
+ 
+    </div>
+ 
+</div>
+ 
+ 
 
  
 <nav class="optionsMenu">
