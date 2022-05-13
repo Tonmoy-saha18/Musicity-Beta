632639a0e66df6ed87fea153e43bf3faae9c1315
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-13 17:56:39 +0600

    deleteplaylist error solved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,147 +1,148 @@
-
 var mouseDown = false;
 var currentIndex = 0;
 var repeat = false;
 var shuffle = false;
 var timer;
 
-$(document).click(function(click) {
-	var target = $(click.target);
+$(document).click(function (click) {
+  var target = $(click.target);
 
-	if(!target.hasClass("item") && !target.hasClass("optionsButton")) {
-		hideOptionsMenu();
-	}
+  if (!target.hasClass("item") && !target.hasClass("optionsButton")) {
+    hideOptionsMenu();
+  }
 });
-$(document).on("change", "select.playlist", function() {
-	var select = $(this);
-	var playlistId = select.val();
-	var songId = select.prev(".songId").val();
+$(document).on("change", "select.playlist", function () {
+  var select = $(this);
+  var playlistId = select.val();
+  var songId = select.prev(".songId").val();
 
-	$.post("includes/handlers/ajax/addToPlaylist.php", { playlistId: playlistId, songId: songId})
-	.done(function(error) {
+  $.post("includes/handlers/ajax/addToPlaylist.php", {
+    playlistId: playlistId,
+    songId: songId,
+  }).done(function (error) {
+    if (error != "") {
+      alert(error);
+      return;
+    }
 
-		if(error != "") {
-			alert(error);
-			return;
-		}
-
-		hideOptionsMenu();
-		select.val("");
-	});
+    hideOptionsMenu();
+    select.val("");
+  });
 });
-$(window).scroll(function() {
-	hideOptionsMenu();
+$(window).scroll(function () {
+  hideOptionsMenu();
 });
 
 function deletePlaylist(playlistId) {
-    var prompt = confirm("Are you sure you want to delete this playlist?");
- 
-    if(prompt == true) {
- 
-        $.post("includes/handlers/ajax/deletePlaylist.php", { playlistId: playlistId })
-        .done(function(error) {
- 
-            if(error != "") {
-                alert(error);
-                return;
-            }
- 
-            //do something when ajax returns
-            openPage("yourMusic.php");
-        });
- 
- 
-    }
-}
- 
-function openPage(url) {
-	if(timer != null) {
-		clearTimeout(timer);
-	}
-	if(url.indexOf("?") == -1) {
-		url = url + "?";
-	}
+  var prompt = confirm("Are you sure you want to delete this playlist?");
 
-	var encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
-	$("#mainContent").load(encodedUrl);
-	$("body").scrollTop(0);
-	history.pushState(null, null, url);
+  if (prompt == true) {
+    $.post("includes/handlers/ajax/deletePlaylist.php", {
+      playlistId: playlistId,
+    }).done(function (error) {
+      //   if (error != "") {
+      //     alert(error);
+      //     return;
+      //   }
+
+      //do something when ajax returns
+      openPage("yourMusic.php");
+    });
+  }
+}
+
+function openPage(url) {
+  if (timer != null) {
+    clearTimeout(timer);
+  }
+  if (url.indexOf("?") == -1) {
+    url = url + "?";
+  }
+
+  var encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
+  $("#mainContent").load(encodedUrl);
+  $("body").scrollTop(0);
+  history.pushState(null, null, url);
 }
 
 function logout() {
-	$.post("includes/handlers/ajax/logout.php", function() {
-		location.reload();
-	});
+  $.post("includes/handlers/ajax/logout.php", function () {
+    location.reload();
+  });
 }
 
 function formatTime(seconds) {
-	var time = Math.round(seconds);
-	var minutes = Math.floor(time / 60); //Rounds down
-	var seconds = time - (minutes * 60);
+  var time = Math.round(seconds);
+  var minutes = Math.floor(time / 60); //Rounds down
+  var seconds = time - minutes * 60;
 
-	var extraZero = (seconds < 10) ? "0" : "";
+  var extraZero = seconds < 10 ? "0" : "";
 
-	return minutes + ":" + extraZero + seconds;
+  return minutes + ":" + extraZero + seconds;
 }
 
 function createPlaylist() {
- 
-    var popup = prompt("Please enter the name of your playlist");
- 
-    if(popup != null) {
- 
-        $.post("includes/handlers/ajax/createPlaylist.php", { name: popup, username: userLoggedIn })
-        .done(function(error) {
- 
-            if(error != "") {
-                alert(error);
-                return;
-            }
- 
-            //do something when ajax returns
-            openPage("yourMusic.php");
-        });
- 
-    }
- 
+  var popup = prompt("Please enter the name of your playlist");
+
+  if (popup != null) {
+    $.post("includes/handlers/ajax/createPlaylist.php", {
+      name: popup,
+      username: userLoggedIn,
+    }).done(function (error) {
+      if (error != "") {
+        alert(error);
+        return;
+      }
+
+      //do something when ajax returns
+      openPage("yourMusic.php");
+    });
+  }
 }
 
 function updateEmail(emailClass) {
-	var emailValue = $("." + emailClass).val();
+  var emailValue = $("." + emailClass).val();
 
-	$.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn})
-	.done(function(response) {
-		$("." + emailClass).nextAll(".message").text(response);
-	})
-
-
+  $.post("includes/handlers/ajax/updateEmail.php", {
+    email: emailValue,
+    username: userLoggedIn,
+  }).done(function (response) {
+    $("." + emailClass)
+      .nextAll(".message")
+      .text(response);
+  });
 }
 
-function updateDescription(descriptionClass){
-	var dValue = $("." + descriptionClass).val();
+function updateDescription(descriptionClass) {
+  var dValue = $("." + descriptionClass).val();
 
-	$.post("includes/handlers/ajax/updateDescription.php", { description: dValue, username: userLoggedIn})
-	.done(function(response) {
-		$("." + descriptionClass).nextAll(".message").text(response);
-	})
+  $.post("includes/handlers/ajax/updateDescription.php", {
+    description: dValue,
+    username: userLoggedIn,
+  }).done(function (response) {
+    $("." + descriptionClass)
+      .nextAll(".message")
+      .text(response);
+  });
 }
 
-function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
-	var oldPassword = $("." + oldPasswordClass).val();
-	var newPassword1 = $("." + newPasswordClass1).val();
-	var newPassword2 = $("." + newPasswordClass2).val();
+function updatePassword(
+  oldPasswordClass,
+  newPasswordClass1,
+  newPasswordClass2
+) {
+  var oldPassword = $("." + oldPasswordClass).val();
+  var newPassword1 = $("." + newPasswordClass1).val();
+  var newPassword2 = $("." + newPasswordClass2).val();
 
-	$.post("includes/handlers/ajax/updatePassword.php", 
-		{ oldPassword: oldPassword,
-			newPassword1: newPassword1,
-			newPassword2: newPassword2, 
-			username: userLoggedIn})
-
-	.done(function(response) {
-		$("." + oldPasswordClass).nextAll(".message").text(response);
-	})
-
-
+  $.post("includes/handlers/ajax/updatePassword.php", {
+    oldPassword: oldPassword,
+    newPassword1: newPassword1,
+    newPassword2: newPassword2,
+    username: userLoggedIn,
+  }).done(function (response) {
+    $("." + oldPasswordClass)
+      .nextAll(".message")
+      .text(response);
+  });
 }
-
-
