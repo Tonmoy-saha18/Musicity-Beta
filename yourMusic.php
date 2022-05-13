5bd46f807756eff190f024bd90a5980da1dc4442
unknown <mahmudyea@gmail.com>
2022-05-13 13:30:59 +0600

    create playlist button added

---
+++ yourMusic.php
@@ -0,0 +1,25 @@
+<?php
+include("includes/includedFiles.php");
+?>
+ 
+<div class="playlistsContainer">
+ 
+    <div class="gridViewContainer">
+        <h2>PLAYLISTS</h2>
+ 
+        <div class="buttonItems">
+            <button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
+        </div>
+ 
+ 
+ 
+      
+ 
+ 
+ 
+ 
+ 
+ 
+    </div>
+ 
+</div>
