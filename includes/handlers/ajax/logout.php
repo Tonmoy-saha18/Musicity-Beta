e7648672fc1c9828d8ec79542c6e7e7d43b4c064
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-13 14:28:33 +0600

    log-out is updated

--- includes/handlers/ajax/logout.php
+++ includes/handlers/ajax/logout.php
@@ -2,18 +2,8 @@
     session_start();
 
     // Checking whelter the admin is logged in or not
-    if(isset($_SESSION['userLoggedIn']) && !empty($_SESSION['userLoggedIn'])){
-        unset($_SESSION['userLoggedIn']); //deleting the session stored  value
 
-        // send it sign in page
-        ?>
-            <script>location.assign('../../login.php')</script>
-        <?php
-    }
-    else{
-        // the user isn't logged in so it also take it to the login.php page
-        ?>
-            <script>location.assign('../../login.php')</script>
-        <?php
-    }
+    unset($_SESSION['userLoggedIn']); //deleting the session stored  value
+
+    // send it sign in page
 ?>
