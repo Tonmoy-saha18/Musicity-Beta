7034e67f5ef536be3097d3832e6be04e17d511aa
enamulrafti <enamul.rafti@gmail.com>
2022-05-13 16:55:06 +0600

    add playlist frontend update 2 _tracklist container mehtod

--- playlist.php
+++ playlist.php
@@ -30,7 +30,54 @@ $owner = new User($con, $playlist->getOwner());
 </div>
  
  
-
+<div class="tracklistContainer">
+    <ul class="tracklist">
+       
+        <?php
+        $songIdArray = $playlist->getSongIds();
+ 
+        $i = 1;
+        foreach($songIdArray as $songId) {
+ 
+            $playlistSong = new Song($con, $songId['song_id']);
+            $songArtist = $playlistSong->getArtist();
+ 
+            echo "<li class='tracklistRow'>
+                    <div class='trackCount'>
+                        <img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $playlistSong->getId() . "\", tempPlaylist, true)'>
+                        <span class='trackNumber'>$i</span>
+                    </div>
+ 
+ 
+                    <div class='trackInfo'>
+                        <span class='trackName'>" . $playlistSong->getTitle() . "</span>
+                        <span class='artistName'>" . $songArtist->getName() . "</span>
+                    </div>
+ 
+                    <div class='trackOptions'>
+                        <input type='hidden' class='songId' value='" . $playlistSong->getId() . "'>
+                        <img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
+                    </div>
+ 
+                    <div class='trackDuration'>
+                        <span class='duration'>" . $playlistSong->getDuration() . "</span>
+                    </div>
+ 
+ 
+                </li>";
+ 
+            $i = $i + 1;
+        }
+ 
+        ?>
+ 
+        <script>
+            var tempSongIds = '<?php echo json_encode(array_column($songIdArray, 'song_id')); ?>';
+            tempPlaylist = JSON.parse(tempSongIds);
+        </script>
+ 
+    </ul>
+</div>
  
 <nav class="optionsMenu">
     <input type="hidden" class="songId">
