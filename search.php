61744c9fc1d8d51d8d1836d70fa57d4160a90b6b
enamulrafti <enamul.rafti@gmail.com>
2022-05-19 01:12:32 +0600

    add search file set caret position

--- search.php
+++ search.php
@@ -40,7 +40,28 @@ $(function() {
     })
  
 })
+function setCaretPosition(ctrl, pos) {
+  // Modern browsers
+  if (ctrl.setSelectionRange) {
+    ctrl.focus();
+    ctrl.setSelectionRange(pos, pos+1);
+ 
+  // IE8 and below
+  } else if (ctrl.createTextRange) {
+    var range = ctrl.createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
+  }
+}
+var input = document.getElementById('searchInput');
+setCaretPosition(input, input.value.length);
+ 
+</script>
+ 
 
+ 
  
  
  
