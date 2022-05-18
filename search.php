0e782b1c62d1a27ee4e08e39f01f6456c57880da
enamulrafti <enamul.rafti@gmail.com>
2022-05-19 01:11:10 +0600

    add search file search input

--- search.php
+++ search.php
@@ -16,8 +16,31 @@ else {
 }
 ?>
  
-
+<div class="searchContainer">
  
+    <h4>Search for an artist, album or song</h4>
+    <input type="text" id="searchInput"class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="this.value = this.value">
+ 
+</div>
+ 
+<script>
+ 
+// $(".searchInput").focus();
+ 
+$(function() {
+ 
+    $(".searchInput").keyup(function() {
+        clearTimeout(timer);
+ 
+        timer = setTimeout(function() {
+            var val = $(".searchInput").val();
+            openPage("search.php?term=" + val);
+        }, 250);
+ 
+    })
+ 
+})
+
  
  
  
