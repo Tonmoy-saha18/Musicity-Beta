a6fc5101f9f3d8e9c32b0abb2a2466158202761c
Tonmoy-Saha18 <tonmoysaha179@gmail.com>
2022-05-19 15:24:13 +0600

    Add song list in artist

--- artist.php
+++ artist.php
@@ -37,7 +37,84 @@ $artist = new Artist($con, $artistId);
 
 </div>
 
+<div class="tracklistContainer borderBottom">
+	<h2>SONGS</h2>
+	<ul class="tracklist">
+		
+		<?php
+		$songIdArray = $artist->getSongIds();
 
+		$i = 1;
+		foreach($songIdArray as $songId) {
+
+			if($i > 5) {
+				break;
+			}
+
+			$albumSong = new Song($con, $songId);
+			$albumArtist = $albumSong->getArtist();
+
+			echo "<li class='tracklistRow'>
+					<div class='trackCount'>
+						<img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
+						<span class='trackNumber'>$i</span>
+					</div>
+
+
+					<div class='trackInfo'>
+						<span class='trackName'>" . $albumSong->getTitle() . "</span>
+						<span class='artistName'>" . $albumArtist->getName() . "</span>
+					</div>
+
+					<div class='trackOptions'>
+						<input type='hidden' class='songId' value='" . $albumSong->getId() . "'>
+					</div>
+					<div>
+						<button class='btn buybtn' name='purchase' onclick='PurchaseSong(" . $albumSong->getId() . ", " . $userLoggedIn->getUserBalance() . ");'> Purchase</button>
+						<button class='btn reportbtn' name='report' onclick='reportSong( " . $userLoggedIn->getUserId() . ", " . $albumSong->getId() . " )'>Report</button>
+					</div>
+					<div class='trackDuration'>
+						<span class='duration'>" . $albumSong->getDuration() . "</span>
+					</div>
+
+
+				</li>";
+
+			$i = $i + 1;
+		}
+
+		?>
+
+		<script>
+			var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+			tempPlaylist = JSON.parse(tempSongIds);
+			function PurchaseSong(song_id,amount){
+                if(amount<30){
+                    alert("You don't have enough money in your account to purchase the song");
+                }
+                else{
+                    var play_name = window.prompt("Enter The Playlist Name Where You want Save That Song");
+                    if(play_name!=null && play_name!=""){
+                        location.assign("includes/handlers/purchase-handler.php?sid="+song_id+"&pname="+play_name);
+                    }
+                    else{
+                        alert("You Haven't enter a valid name")
+                    }
+                }
+            }
+            function reportSong(userid,song_id){
+				var message = prompt("Why do you want to report this song?");
+				if(message != null && message != "") {
+					location.assign("includes/handlers/report-handler.php?userid="+userid+"&songid="+song_id+"&msg="+message);
+				}
+				else{
+					alert("Please tell us why do want to report this song")
+				}
+            }
+		</script>
+
+	</ul>
+</div>
 
 
 <div class="gridViewContainer">
@@ -70,3 +147,7 @@ $artist = new Artist($con, $artistId);
 
 </div>
 
+<nav class="optionsMenu">
+	<input type="hidden" class="songId">
+	<?php echo Playlist::getPlaylistsDropdown($con, $userLoggedIn->getUsername()); ?>
+</nav>
