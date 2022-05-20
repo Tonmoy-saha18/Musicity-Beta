1fae625f91767fd591da599eb2e1a8e4d6cae497
rsnila <adritanila64658@gmail.com>
2022-05-20 16:04:21 +0600

    like song ajax updated added

--- includes/handlers/ajax/likeSong.php
+++ includes/handlers/ajax/likeSong.php
@@ -3,7 +3,18 @@ session_start();
 include("../../config.php");
  
 if(isset($_POST['podId'])) {
-   
+    $playlistId = $_POST['podId'];
+ 
+    $username = $_SESSION['userLoggedIn'];
+    $query = "SELECT * FROM users WHERE username='$username'";
+    $returnobj = $con->query($query);
+    $user = $returnobj->fetchAll();
+    foreach ($user as $us) {
+        $user_id = $us['id'];
+    }
+ 
+    $likeQuery = "INSERT INTO likedsong VALUES(NULL, $playlistId, $user_id)";
+    $con->exec($likeQuery);
  
 }
 else {
