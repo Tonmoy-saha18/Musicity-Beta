c68ffb8d74f88ee45223fe55766500fddd27655f
rsnila <adritanila64658@gmail.com>
2022-05-20 16:06:21 +0600

    unlike song ajax updated final added

--- includes/handlers/ajax/unlikeSong.php
+++ includes/handlers/ajax/unlikeSong.php
@@ -3,7 +3,17 @@ session_start();
 include("../../config.php");
  
 if(isset($_POST['podId'])) {
-   
+    $playlistId = $_POST['podId'];
+    //taking user id
+    $username = $_SESSION['userLoggedIn'];
+    $query = "SELECT * FROM users WHERE username='$username'";
+    $returnobj = $con->query($query);
+    $user = $returnobj->fetchAll();
+    foreach ($user as $us) {
+        $user_id = $us['id'];
+    }
+    $likeQuery = "DELETE FROM likedsong WHERE song_id=$playlistId AND userid=$user_id";
+    $con->exec($likeQuery);
  
 }
 else {
